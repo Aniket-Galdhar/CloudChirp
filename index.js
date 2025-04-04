e6d6c846696cb336498940b9ee1df18b4b05d38c
Aniket Galdhar <anigal@Anikets-MacBook-Air-2.local>
2025-04-04 20:46:30 +0530

    Add dotenv for environment variables

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const app = express();
+const dotenv = require('dotenv');
 
+dotenv.config();
+
+// basic route
 app.get('/', (req,res) => {
     res.send('Server is running');
 });
 
+// start server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log('Server is running on port ${PORT}');
+    console.log(`Server is running on port ${PORT}`);
 });
