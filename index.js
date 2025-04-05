941ffcd2ada6bb73668fcad0665dc95141b23517
Aniket Galdhar <anigal@Anikets-MacBook-Air-2.local>
2025-04-05 13:36:40 +0530

    Integrate HTTP server and Socket.io

--- index.js
+++ index.js
@@ -1,16 +1,33 @@
 const express = require('express');
 const app = express();
 const dotenv = require('dotenv');
+const http = require('http');
+const socketio = require('socket.io');
 
 dotenv.config();
+const server = http.createServer(app);
+const io = socketio(server);
 
 // basic route
 app.get('/', (req,res) => {
     res.send('Server is running');
 });
 
+io.on('connection',(socket) => {
+    console.log('User connected:', socket.id);
+
+    socket.on('chat message', (msg) => {
+        console.log('Message:', msg);
+        io.emit('chat message', msg);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('User disconnected:', socket.id);
+    });
+});
+
 // start server
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+server.listen(PORT, () => {
+    console.log(`Server is is running on port ${PORT}`);
 });
