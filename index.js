aa685ed812d34103cd65c7a438ecf45036aaa0e5
Aniket Galdhar <anigal@Anikets-MacBook-Air-2.local>
2025-04-05 15:17:17 +0530

    Add Express + Socket.io server with static frontend and MongoDB setup

--- index.js
+++ index.js
@@ -4,16 +4,16 @@ const dotenv = require('dotenv');
 const http = require('http');
 const socketio = require('socket.io');
 const mongoose = require('mongoose');
+const path = require('path');
 
 dotenv.config();
 const server = http.createServer(app);
 const io = socketio(server);
 
-// basic route
-app.get('/', (req,res) => {
-    res.send('Server is running');
-});
+// serve static files (HTML/CSS/JS)
+app.use(express.static(path.join(__dirname,'public')));
 
+// socket.io setup
 io.on('connection',(socket) => {
     console.log('User connected:', socket.id);
 
@@ -27,9 +27,16 @@ io.on('connection',(socket) => {
     });
 });
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-        .then(() => console.log('MongoDB connected'))
-        .catch((err) => console.error('MongoDB connection error:', err));
+
+// mongo db connection
+mongoose.connect(process.env.MONGO_URI, { 
+    useNewUrlParser: true, useUnifiedTopology: true 
+}).then(() => {
+    console.log('MonogDB connected');
+}).catch((err) => {
+    console.error('MongoDB connection error',err);
+});
+
 
 // start server
 const PORT = process.env.PORT || 3000;
